7f3f784f097ebca6ea6295a3b504ac78e9d56b3c
PrIzRaKDev <rockstarmixplayne@gmail.com>
2024-06-29 18:03:09 +0300

    Update 1.2.0-alpha

--- VorbisFile/dllmain.cpp
+++ VorbisFile/dllmain.cpp
@@ -7,8 +7,10 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#define MAX_PATH 512
 
 using namespace std;
+
 
 // Global variables for storing loaded modules and log file
 std::vector<HMODULE> loadedASIModules;
@@ -16,7 +18,7 @@ std::vector<HMODULE> loadedDLLModules;
 std::wofstream logFile;
 
 // Function to get list of .asi files
-static void __fastcall GetASIFiles(const std::wstring& directory, std::vector<std::wstring>& out) {
+void GetASIFiles(const std::wstring& directory, std::vector<std::wstring>& out) {
     WIN32_FIND_DATAW findFileData;
     HANDLE hFind = FindFirstFileW((directory + L"\\*.asi").c_str(), &findFileData);
 
@@ -30,7 +32,7 @@ static void __fastcall GetASIFiles(const std::wstring& directory, std::vector<st
     }
 }
 
-static void __fastcall GetDLLFiles(const std::wstring& directory, std::vector<std::wstring>& out) {
+void GetDLLFiles(const std::wstring& directory, std::vector<std::wstring>& out) {
     WIN32_FIND_DATAW findFileData;
     HANDLE hFind = FindFirstFileW((directory + L"\\*.dll").c_str(), &findFileData);
 
@@ -47,20 +49,51 @@ static void __fastcall GetDLLFiles(const std::wstring& directory, std::vector<st
 // Function to write messages to log file
 static void WriteToLogFile(const std::wstring& message) {
     if (!logFile.is_open()) {
-        logFile.open(L"asi_loader.log", std::ios_base::app);
-    }
-    if (logFile.is_open()) {
+        logFile.open(L"ASI_Loader.log", std::ios_base::app);
         logFile << message << std::endl;
-    } else {
+    }
+    else if (logFile.is_open())
+    {
+        logFile << message << std::endl;
+    }
+    else 
+    {
         std::wcerr << L"Failed to open log file." << std::endl;
+        return;
     }
 }
 
-// Function to initialize ASI loader
-void __fastcall InitializeASILoader() {
-        
+static void exeUnprotect()
+{
+    /* This "tool" using for unprotecting any code regions in game executable file, for correct writing and/or reading memory sections. */
+            auto hExecutableInstance = (size_t)GetModuleHandle(NULL);
+            IMAGE_NT_HEADERS* ntHeader = (IMAGE_NT_HEADERS*)(hExecutableInstance + ((IMAGE_DOS_HEADER*)hExecutableInstance)->e_lfanew);
+            SIZE_T size = ntHeader->OptionalHeader.SizeOfImage;
+            DWORD oldProtect;
+	    VirtualProtect((VOID*)hExecutableInstance, size, PAGE_EXECUTE_READWRITE, &oldProtect);
+            if (!VirtualProtect((VOID*)hExecutableInstance, size, PAGE_EXECUTE_READWRITE, &oldProtect))
+		{
+		   MessageBoxW(nullptr, L"Не удалось сделать unprotect!", L"Ошибка!", MB_OK);
+		} 
+		else 
+		{
+		   MessageBoxW(nullptr, L"DebugMsg: exeUnprotect успешно выполнен!", L"Успешно! Debug", MB_OK);
+		   WriteToLogFile(L"Exe unprotected successfully!");
+		}	
 
-    std::wstring MessageINIT = L"ASI Loader v.1.1 by Pr!zRaK initialized! \n";
+}
+
+
+
+// Function to initialize ASI loader
+static void InitializeASILoader() {
+
+    if (logFile.is_open())
+    {
+        logFile.clear();
+    }
+    
+    std::wstring MessageINIT = L"ASI Loader v.1.2.0 by Pr!zRaK initialized! \n";
     WriteToLogFile(MessageINIT);
 
     wchar_t modulePath[MAX_PATH];
@@ -75,7 +108,7 @@ void __fastcall InitializeASILoader() {
     GetDLLFiles(DLLDirectory, dllFiles);
 
     for (const auto& ASIFile : asiFiles) {
-        HMODULE ASIHandle = LoadLibraryExW(ASIFile.c_str(), nullptr, 0x00000008);
+        HMODULE ASIHandle = LoadLibraryExW(ASIFile.c_str(), nullptr, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
         if (ASIHandle != nullptr) {
             loadedASIModules.push_back(ASIHandle);
             std::wstring Messagetrue = L"Plugin " + ASIFile + L" loaded! \n";
@@ -103,15 +136,13 @@ void __fastcall InitializeASILoader() {
 }
 
 // Function to free resources
-void FreeResources() {
-    for (HMODULE &asiModule : loadedASIModules) {
-	delete[] asiModule;
+static void FreeResources() {
+    for (HMODULE asiModule : loadedASIModules) {
 	FreeLibrary(asiModule);
     }
     loadedASIModules.clear();
 
-    for (HMODULE &dllModule : loadedDLLModules) {
-	delete[] dllModule;
+    for (HMODULE dllModule : loadedDLLModules) {
 	FreeLibrary(dllModule);
     }
     loadedDLLModules.clear();
@@ -122,12 +153,14 @@ void FreeResources() {
 }
 
 // DLL entry point
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+static BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
             DisableThreadLibraryCalls(hModule); // Disable DLL_THREAD_ATTACH and DLL_THREAD_DETACH calls
             InitializeASILoader();
-	    MessageBoxW(nullptr, L"ASI Loader 1.1 Успешно иниацилизирован", L" ", MB_OK);
+	    exeUnprotect();
+	        MessageBoxW(nullptr, L"ASI Loader 1.2.0 Успешно иниацилизирован!", L" ", MB_OK);
+		
             break;
         case DLL_PROCESS_DETACH:
             FreeResources();
